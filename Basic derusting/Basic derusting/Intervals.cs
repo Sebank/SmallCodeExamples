6e364cb52840f135d142e79e271a284b7d71d25b
Sebastian Ankill <Sebastian.Ankill@nhn.no>
2024-05-02 12:52:22 +0200

    found non overlapping intervals

---
+++ Basic derusting/Basic derusting/Intervals.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_derusting
+{
+    public static class Intervals
+    {
+        public static int FindNonOverlappingIntervals(List<int[]> intervals)
+        {
+            Dictionary<int, List<int>> indexOfConflicts = new Dictionary<int, List<int>>();
+
+            for ( int i = 0; i < intervals.Count; i++ )
+            {
+                for ( int j = i + 1; j < intervals.Count; j++)
+                {
+                    /* Note that we have three cases, 
+                     * when we consider two intervals [a, b], [c, d]
+                     * a < b && b < c < d
+                     * a < b < c && a < d < c
+                     * a < b < c && c < d
+                     * Alternatively we can check if both start and end
+                     * is before or after start of the other interval
+                     */
+                    if ( !(intervals[i][0] <= intervals[j][0] &&
+                           intervals[i][1] <= intervals[j][0]) 
+                         && 
+                         !(intervals[i][0] >= intervals[j][1] &&
+                           intervals[i][1] >= intervals[j][1])
+                       )
+                    {
+                        if(!indexOfConflicts.TryGetValue(i, out List<int> conflictsForI))
+                        {
+                            conflictsForI = new List<int>();
+                            indexOfConflicts[i] = conflictsForI;
+                        }
+                        conflictsForI.Add(j);
+                        if (!indexOfConflicts.TryGetValue(j, out List<int> conflictsForJ))
+                        {
+                            conflictsForJ = new List<int>();
+                            indexOfConflicts[j] = conflictsForJ;
+                        }
+                        conflictsForJ.Add(i);
+                    }
+
+                }
+            }
+            return indexOfConflicts.Values.Min(e => e.Count);
+        }
+    }
+}
--- Basic derusting/Basic derusting/Program.cs
+++ Basic derusting/Basic derusting/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine("reverse Words: " + stringManipulation.ReverseWordInString(inp
 Console.WriteLine("reverse Vowels: " + stringManipulation.ReverseVowelsInString(input));
 
 Console.WriteLine(stringManipulation.GreatestCommonSequenceInString("ABABAB", "ABAB"));
+
+Console.WriteLine("find non overlapping intervals: " + Intervals.FindNonOverlappingIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]));
